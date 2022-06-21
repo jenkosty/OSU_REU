316a5c5f9c55299c9748867be48b0d34f85a6ba0
jenkosty <82013074+jenkosty@users.noreply.github.com>
2022-06-20 22:02:14 -0700

    Update OOI_Time_Series.m

--- OOI_Time_Series.m
+++ OOI_Time_Series.m
@@ -16,10 +16,15 @@ oism.temp1m = readtable('OOI_CE_OISM_SB_Temperature.csv');
 oism.salt1m = readtable('OOI_CE_OISM_SB_Salinity.csv');
 oism.temp7m = readtable('OOI_CE_OISM_NSIF_Temperature.csv');
 oism.salt7m = readtable('OOI_CE_OISM_NSIF_Salinity.csv');
+oism.pco2_7m = readtable('OOI_CE_OISM_NSIF_pCO2.csv');
+oism.do7m = readtable('OOI_CE_OISM_NSIF_DO.csv');
 oism.temp25m = readtable('OOI_CE_OISM_SMFN_Temperature.csv');
 oism.salt25m = readtable('OOI_CE_OISM_SMFN_Salinity.csv');
+oism.pco2_25m = readtable('OOI_CE_OISM_SMFN_pCO2.csv');
+oism.do25m = readtable('OOI_CE_OISM_SMFN_DO.csv');
 
 %%% Filtering OISM to the highest quality control indicators
+%%% Note: DO and pCO2 data do not have quality control indicators
 oism.temp1m = oism.temp1m(oism.temp1m.sea_water_temperature_qc_agg == 1, :);
 oism.salt1m = oism.salt1m(oism.salt1m.sea_water_practical_salinity_qc_agg == 1, :);
 oism.temp7m = oism.temp7m(oism.temp7m.sea_water_temperature_qc_agg == 1, :);
@@ -32,26 +37,37 @@ oism.temp1m.datetime = datetime(vertcat(oism.temp1m.time), 'InputFormat', 'yyyy-
 oism.salt1m.datetime = datetime(vertcat(oism.salt1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oism.temp7m.datetime = datetime(vertcat(oism.temp7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oism.salt7m.datetime = datetime(vertcat(oism.salt7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oism.pco2_7m.datetime = datetime(vertcat(oism.pco2_7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oism.do7m.datetime = datetime(vertcat(oism.do7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oism.temp25m.datetime = datetime(vertcat(oism.temp25m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oism.salt25m.datetime = datetime(vertcat(oism.salt25m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oism.pco2_25m.datetime = datetime(vertcat(oism.pco2_25m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oism.do25m.datetime = datetime(vertcat(oism.do25m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 
 %%% Renaming variables
 oism.temp1m = renamevars(oism.temp1m,'sea_water_temperature','temp');
 oism.salt1m = renamevars(oism.salt1m,'sea_water_practical_salinity','salt');
 oism.temp7m = renamevars(oism.temp7m,'sea_water_temperature','temp');
 oism.salt7m = renamevars(oism.salt7m,'sea_water_practical_salinity','salt');
+oism.pco2_7m = renamevars(oism.pco2_7m,'partial_pressure_of_carbon_dioxide_in_sea_water','pco2');
+oism.do7m = renamevars(oism.do7m,'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water','do');
 oism.temp25m = renamevars(oism.temp25m,'sea_water_temperature','temp');
 oism.salt25m = renamevars(oism.salt25m,'sea_water_practical_salinity','salt');
+oism.pco2_25m = renamevars(oism.pco2_25m,'partial_pressure_of_carbon_dioxide_in_sea_water','pco2');
+oism.do25m = renamevars(oism.do25m,'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water','do');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Loading OSSM data at different depths %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ossm.temp1m = readtable('OOI_CE_OOSM_SB_Temperature.csv');
-ossm.salt1m = readtable('OOI_CE_OOSM_SB_Salinity.csv');
-ossm.temp7m = readtable('OOI_CE_OOSM_NSIF_Temperature.csv');
-ossm.salt7m = readtable('OOI_CE_OOSM_NSIF_Salinity.csv');
+ossm.temp1m = readtable('OOI_CE_OSSM_SB_Temperature.csv');
+ossm.salt1m = readtable('OOI_CE_OSSM_SB_Salinity.csv');
+ossm.pco2_1m = readtable('OOI_CE_OSSM_SB_pCO2.csv');
+ossm.temp7m = readtable('OOI_CE_OSSM_NSIF_Temperature.csv');
+ossm.salt7m = readtable('OOI_CE_OSSM_NSIF_Salinity.csv');
+ossm.do7m = readtable('OOI_CE_OSSM_NSIF_DO.csv');
 
 %%% Filtering OSSM to the highest quality control indicators
+%%% Note: DO and pCO2 data do not have quality control indicators
 ossm.temp1m = ossm.temp1m(ossm.temp1m.sea_surface_temperature_qc_agg == 1, :);
 ossm.salt1m = ossm.salt1m(ossm.salt1m.sea_water_practical_salinity_qc_agg == 1, :);
 ossm.temp7m = ossm.temp7m(ossm.temp7m.sea_water_temperature_qc_agg == 1, :);
@@ -60,24 +76,31 @@ ossm.salt7m = ossm.salt7m(ossm.salt7m.sea_water_practical_salinity_qc_agg == 1, 
 %%% Converting time strings to datetime
 ossm.temp1m.datetime = datetime(vertcat(ossm.temp1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 ossm.salt1m.datetime = datetime(vertcat(ossm.salt1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+ossm.pco2_1m.datetime = datetime(vertcat(ossm.pco2_1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 ossm.temp7m.datetime = datetime(vertcat(ossm.temp7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 ossm.salt7m.datetime = datetime(vertcat(ossm.salt7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+ossm.do7m.datetime = datetime(vertcat(ossm.do7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 
 %%% Renaming variables
 ossm.temp1m = renamevars(ossm.temp1m,'sea_surface_temperature','temp');
 ossm.salt1m = renamevars(ossm.salt1m,'sea_water_practical_salinity','salt');
+ossm.pco2_1m = renamevars(ossm.pco2_1m,'surface_partial_pressure_of_carbon_dioxide_in_sea_water','pco2');
 ossm.temp7m = renamevars(ossm.temp7m,'sea_water_temperature','temp');
 ossm.salt7m = renamevars(ossm.salt7m,'sea_water_practical_salinity','salt');
+ossm.do7m = renamevars(ossm.do7m,'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water','do');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Loading OOSM data at different depths %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 oosm.temp1m = readtable('OOI_CE_OOSM_SB_Temperature.csv');
 oosm.salt1m = readtable('OOI_CE_OOSM_SB_Salinity.csv');
+oosm.pco2_1m = readtable('OOI_CE_OOSM_SB_pCO2.csv');
 oosm.temp7m = readtable('OOI_CE_OOSM_NSIF_Temperature.csv');
 oosm.salt7m = readtable('OOI_CE_OOSM_NSIF_Salinity.csv');
+oosm.do7m = readtable('OOI_CE_OOSM_NSIF_DO.csv');
 
 %%% Filtering OOSM to the highest quality control indicators
+%%% Note: DO and pCO2 data do not have quality control indicators
 oosm.temp1m = oosm.temp1m(oosm.temp1m.sea_surface_temperature_qc_agg == 1, :);
 oosm.salt1m = oosm.salt1m(oosm.salt1m.sea_water_practical_salinity_qc_agg == 1, :);
 oosm.temp7m = oosm.temp7m(oosm.temp7m.sea_water_temperature_qc_agg == 1, :);
@@ -86,14 +109,18 @@ oosm.salt7m = oosm.salt7m(oosm.salt7m.sea_water_practical_salinity_qc_agg == 1, 
 %%% Converting time strings to datetime
 oosm.temp1m.datetime = datetime(vertcat(oosm.temp1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oosm.salt1m.datetime = datetime(vertcat(oosm.salt1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oosm.pco2_1m.datetime = datetime(vertcat(oosm.pco2_1m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oosm.temp7m.datetime = datetime(vertcat(oosm.temp7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 oosm.salt7m.datetime = datetime(vertcat(oosm.salt7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
+oosm.do7m.datetime = datetime(vertcat(oosm.do7m.time), 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss''Z');
 
 %%% Renaming variables
 oosm.temp1m = renamevars(oosm.temp1m,'sea_surface_temperature','temp');
 oosm.salt1m = renamevars(oosm.salt1m,'sea_water_practical_salinity','salt');
+oosm.pco2_1m = renamevars(oosm.pco2_1m,'surface_partial_pressure_of_carbon_dioxide_in_sea_water','pco2');
 oosm.temp7m = renamevars(oosm.temp7m,'sea_water_temperature','temp');
 oosm.salt7m = renamevars(oosm.salt7m,'sea_water_practical_salinity','salt');
+oosm.do7m = renamevars(oosm.do7m,'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water','do');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%% Loading and pre-processing meterological data %%%%%%%%%%%%%%
@@ -224,40 +251,48 @@ clear riverflow_unfmt
 max_gap = hours(3);
 
 %%% Interpolating OISM data onto time grid
-oism.time_interp = time_grid;
+oism.datetime_interp = time_grid;
 oism.temp1m_interp = interp1gap(datenum(oism.temp1m.datetime), oism.temp1m.temp, datenum(time_grid), datenum(max_gap));
 oism.salt1m_interp = interp1gap(datenum(oism.salt1m.datetime), oism.salt1m.salt, datenum(time_grid), datenum(max_gap));
 oism.temp7m_interp = interp1gap(datenum(oism.temp7m.datetime), oism.temp7m.temp, datenum(time_grid), datenum(max_gap));
 oism.salt7m_interp = interp1gap(datenum(oism.salt7m.datetime), oism.salt7m.salt, datenum(time_grid), datenum(max_gap));
+oism.pco2_7m_interp = interp1gap(datenum(oism.pco2_7m.datetime), oism.pco2_7m.pco2, datenum(time_grid), datenum(max_gap));
+oism.do7m_interp = interp1gap(datenum(oism.do7m.datetime), oism.do7m.do, datenum(time_grid), datenum(max_gap));
 oism.temp25m_interp = interp1gap(datenum(oism.temp25m.datetime), oism.temp25m.temp, datenum(time_grid), datenum(max_gap));
 oism.salt25m_interp = interp1gap(datenum(oism.salt25m.datetime), oism.salt25m.salt, datenum(time_grid), datenum(max_gap));
+oism.pco2_25m_interp = interp1gap(datenum(oism.pco2_25m.datetime), oism.pco2_25m.pco2, datenum(time_grid), datenum(max_gap));
+oism.do25m_interp = interp1gap(datenum(oism.do25m.datetime), oism.do25m.do, datenum(time_grid), datenum(max_gap));
 
 %%% Interpolating OOSM data onto time grid
-ossm.time_interp = time_grid;
+ossm.datetime_interp = time_grid;
 ossm.temp1m_interp = interp1gap(datenum(ossm.temp1m.datetime), ossm.temp1m.temp, datenum(time_grid), datenum(max_gap));
 ossm.salt1m_interp = interp1gap(datenum(ossm.salt1m.datetime), ossm.salt1m.salt, datenum(time_grid), datenum(max_gap));
+ossm.pco2_1m_interp = interp1gap(datenum(ossm.pco2_1m.datetime), ossm.pco2_1m.pco2, datenum(time_grid), datenum(max_gap));
 ossm.temp7m_interp = interp1gap(datenum(ossm.temp7m.datetime), ossm.temp7m.temp, datenum(time_grid), datenum(max_gap));
 ossm.salt7m_interp = interp1gap(datenum(ossm.salt7m.datetime), ossm.salt7m.salt, datenum(time_grid), datenum(max_gap));
+ossm.do7m_interp = interp1gap(datenum(ossm.do7m.datetime), ossm.do7m.do, datenum(time_grid), datenum(max_gap));
 
 %%% Interpolating OOSM data onto time grid
-oosm.time_interp = time_grid;
+oosm.datetime_interp = time_grid;
 oosm.temp1m_interp = interp1gap(datenum(oosm.temp1m.datetime), oosm.temp1m.temp, datenum(time_grid), datenum(max_gap));
 oosm.salt1m_interp = interp1gap(datenum(oosm.salt1m.datetime), oosm.salt1m.salt, datenum(time_grid), datenum(max_gap));
+oosm.pco2_1m_interp = interp1gap(datenum(oosm.pco2_1m.datetime), oosm.pco2_1m.pco2, datenum(time_grid), datenum(max_gap));
 oosm.temp7m_interp = interp1gap(datenum(oosm.temp7m.datetime), oosm.temp7m.temp, datenum(time_grid), datenum(max_gap));
 oosm.salt7m_interp = interp1gap(datenum(oosm.salt7m.datetime), oosm.salt7m.salt, datenum(time_grid), datenum(max_gap));
+oosm.do7m_interp = interp1gap(datenum(oosm.do7m.datetime), oosm.do7m.do, datenum(time_grid), datenum(max_gap));
 
 %%% Interpolating shelf wind data onto time grid
-metero_shelf.time_interp = time_grid;
+metero_shelf.datetime_interp = time_grid;
 metero_shelf.wind_dir_interp = interp1gap(datenum(metero_shelf.datetime), metero_shelf.wind_dir, datenum(time_grid), datenum(max_gap));
 metero_shelf.wind_spd_interp = interp1gap(datenum(metero_shelf.datetime), metero_shelf.wind_spd, datenum(time_grid), datenum(max_gap));
 
 %%% Interpolating offshore wind data onto time grid
-metero_offshore.time_interp = time_grid;
+metero_offshore.datetime_interp = time_grid;
 metero_offshore.wind_dir_interp = interp1gap(datenum(metero_offshore.datetime), metero_offshore.wind_dir, datenum(time_grid), datenum(max_gap));
 metero_offshore.wind_spd_interp = interp1gap(datenum(metero_offshore.datetime), metero_offshore.wind_spd, datenum(time_grid), datenum(max_gap));
 
 %%% Interpolating river discharge data
-riverflow.time_interp = time_grid;
+riverflow.datetime_interp = time_grid;
 riverflow.flow_interp = interp1gap(datenum(riverflow.datetime), riverflow.flow, datenum(time_grid), datenum(max_gap));
     
 %%
@@ -269,40 +304,48 @@ riverflow.flow_interp = interp1gap(datenum(riverflow.datetime), riverflow.flow, 
 runmean_time_step = datenum(hours(1));
 
 %%% Creating running mean for the OISM data on time grid
-oism.time_runmean = time_grid;
+oism.datetime_runmean = time_grid;
 oism.temp1m_runmean = runmean(time_grid, runmean_time_step, oism.temp1m.datetime, oism.temp1m.temp);
 oism.salt1m_runmean = runmean(time_grid, runmean_time_step, oism.salt1m.datetime, oism.salt1m.salt);
 oism.temp7m_runmean = runmean(time_grid, runmean_time_step, oism.temp7m.datetime, oism.temp7m.temp);
 oism.salt7m_runmean = runmean(time_grid, runmean_time_step, oism.salt7m.datetime, oism.salt7m.salt);
+oism.pco2_7m_runmean = runmean(time_grid, runmean_time_step, oism.pco2_7m.datetime, oism.pco2_7m.pco2);
+oism.do7m_runmean = runmean(time_grid, runmean_time_step, oism.do7m.datetime, oism.do7m.do);
 oism.temp25m_runmean = runmean(time_grid, runmean_time_step, oism.temp25m.datetime, oism.temp25m.temp);
 oism.salt25m_runmean = runmean(time_grid, runmean_time_step, oism.salt25m.datetime, oism.salt25m.salt);
+oism.pco2_25m_runmean = runmean(time_grid, runmean_time_step, oism.pco2_25m.datetime, oism.pco2_25m.pco2);
+oism.do25m_runmean = runmean(time_grid, runmean_time_step, oism.do25m.datetime, oism.do25m.do);
 
 %%% Creating running mean for the OSSM data on time grid
-ossm.time_runmean = time_grid;
+ossm.datetime_runmean = time_grid;
 ossm.temp1m_runmean = runmean(time_grid, runmean_time_step, ossm.temp1m.datetime, ossm.temp1m.temp);
 ossm.salt1m_runmean = runmean(time_grid, runmean_time_step, ossm.salt1m.datetime, ossm.salt1m.salt);
+ossm.pco2_1m_runmean = runmean(time_grid, runmean_time_step, ossm.pco2_1m.datetime, ossm.pco2_1m.pco2);
 ossm.temp7m_runmean = runmean(time_grid, runmean_time_step, ossm.temp7m.datetime, ossm.temp7m.temp);
 ossm.salt7m_runmean = runmean(time_grid, runmean_time_step, ossm.salt7m.datetime, ossm.salt7m.salt);
+ossm.do7m_runmean = runmean(time_grid, runmean_time_step, ossm.do7m.datetime, ossm.do7m.do);
 
 %%% Creating running mean for the OOSM data on time grid
-oosm.time_runmean = time_grid;
+oosm.datetime_runmean = time_grid;
 oosm.temp1m_runmean = runmean(time_grid, runmean_time_step, oosm.temp1m.datetime, oosm.temp1m.temp);
 oosm.salt1m_runmean = runmean(time_grid, runmean_time_step, oosm.salt1m.datetime, oosm.salt1m.salt);
+oosm.pco2_1m_runmean = runmean(time_grid, runmean_time_step, oosm.pco2_1m.datetime, oosm.pco2_1m.pco2);
 oosm.temp7m_runmean = runmean(time_grid, runmean_time_step, oosm.temp7m.datetime, oosm.temp7m.temp);
 oosm.salt7m_runmean = runmean(time_grid, runmean_time_step, oosm.salt7m.datetime, oosm.salt7m.salt);
+oosm.do7m_runmean = runmean(time_grid, runmean_time_step, oosm.do7m.datetime, oosm.do7m.do);
 
 %%% Creating running mean for shelf wind data on time grid
-metero_shelf.time_runmean = time_grid;
+metero_shelf.datetime_runmean = time_grid;
 metero_shelf.wind_dir_runmean = runmean(time_grid, runmean_time_step, metero_shelf.datetime, metero_shelf.wind_dir);
 metero_shelf.wind_spd_runmean = runmean(time_grid, runmean_time_step, metero_shelf.datetime, metero_shelf.wind_spd);
 
 %%% Creating running mean for offshore wind data on time grid
-metero_offshore.time_runmean = time_grid;
+metero_offshore.datetime_runmean = time_grid;
 metero_offshore.wind_dir_runmean = runmean(time_grid, runmean_time_step, metero_offshore.datetime, metero_offshore.wind_dir);
 metero_offshore.wind_spd_runmean = runmean(time_grid, runmean_time_step, metero_offshore.datetime, metero_offshore.wind_spd);
 
 %%% Creating running mean for river discharge data on time grid
-riverflow.time_runmean = time_grid;
+riverflow.datetime_runmean = time_grid;
 riverflow.flow_runmean = runmean(time_grid, runmean_time_step, riverflow.datetime, riverflow.flow);
 
 %%
