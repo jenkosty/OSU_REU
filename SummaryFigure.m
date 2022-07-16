d44cc22e0fd3e7753244549f94054c5ab2549cac
jenkosty <82013074+jenkosty@users.noreply.github.com>
2022-07-15 17:05:22 -0700

    Update SummaryFigure.m

--- SummaryFigure.m
+++ SummaryFigure.m
@@ -1,61 +1,41 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%% Loading and pre-processing OISM data %%%%%%%%%%%%%%%%%%%
+=%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%% Loading and pre-processing data %%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% Loading OISM data
 load('OOI_CE_OISM.mat');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%% Loading and pre-processing Yaquina station data %%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Loading Yaquina time series
+load('YaquinaTS.mat');
 
-%%% Loading station data
-load('HFhightide.mat')
-
-%%% Formatting in a structure
-yaquina_HT.datetime = tm;
-yaquina_HT.temp = T;
-yaquina_HT.temp_std = Tsd;
-yaquina_HT.salt = S;
-yaquina_HT.salt_std = Ssd;
-
-clear S Ssd T tm Tsd 
-
-%%% Loading station data
-load('HFsurface.mat')
-
-%%% Formatting in a structure
-yaquina_all.datetime = tm;
-yaquina_all.temp = T;
-yaquina_all.salt = S;
-yaquina_all.cond = C;
-yaquina_all.pres = P;
-
-clear S T tm C P
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%% Loading and Pre-Processing CTD Cast Data %%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%% Loading cast data
+%%% Loading CTD Cast data
 load('HatfieldCTDcasts.mat')
 
 %%% Extracting high tide casts
 casts_HT = casts_final(vertcat(casts_final.TimeToClosestHighTide) < hours(1));
 
+%%% Loading tide data
+load('HourlyTides.mat');
+
+%%% Loading Yaquina River discharge data
+load('YaquinaRiverDischarge.mat');
+
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating Plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
-figure('Renderer', 'painters', 'Position', [100 100 1200 700])
-sgtitle('CTD Casts Taken Within 2 Hours of High Tide')
 
-%%% Temperature subplot
-subplot(211)
+figure('Renderer', 'painters', 'Position', [100 100 1200 1000])
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Temperature subplot %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ax1 = subplot(411);
 hold on
-a = plot(yaquina_all.datetime, yaquina_all.temp, 'Color', [.7 .7 .7], 'DisplayName', 'Hatfield - Full Resolution');
-b = plot(oism.temp1m.datetime, oism.temp1m.temp, 'Color', [68 68 253]./255, 'DisplayName', 'OISM - 1m');
-c = plot(yaquina_HT.datetime, yaquina_HT.temp, 'k', 'DisplayName', 'Hatfield - High Tide');
+a = plot(yaquina_all.datetime, yaquina_all.temp, 'Color', [.7 .7 .7], 'DisplayName', 'Hatfield Full Resolution');
+b = plot(oism.temp1m.datetime, oism.temp1m.temp, 'Color', [68 68 253]./255, 'DisplayName', 'OISM (1m)');
+c = plot(yaquina_HT.datetime, yaquina_HT.temp, 'k', 'DisplayName', 'Hatfield High Tide');
 for i = 1:length(casts_HT)   
 
     %%% Plotting high tide cast
@@ -78,16 +58,19 @@ end
 hold off
 xlim([datetime(2020,12,1), datetime(2022,6,1)])
 ylabel('Temperature (degC)')
-legend([a b c d e f]);
+legend([a b c d e f], 'Location', 'southwest', 'Orientation', 'horizontal');
 
-clear cast_top_temp cast_bottom_temp cast_top cast_bottom a b
+clear cast_top_temp cast_bottom_temp cast_top cast_bottom a b c d e f
 
-%%% Salinity subplot
-subplot(212)
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Salinity subplot %%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+ax2 = subplot(412);
 hold on
-a = plot(yaquina_all.datetime, yaquina_all.salt, 'Color', [.7 .7 .7], 'DisplayName', 'Hatfield - Full Resolution');
-b = plot(oism.salt1m.datetime, oism.salt1m.salt, 'Color', [68 68 253]./255, 'DisplayName', 'OISM - 1m');
-c = plot(yaquina_HT.datetime, yaquina_HT.salt, 'k', 'DisplayName', 'Hatfield - High Tide');
+a = plot(yaquina_all.datetime, yaquina_all.salt, 'Color', [.7 .7 .7], 'DisplayName', 'Hatfield Full Resolution');
+b = plot(oism.salt1m.datetime, oism.salt1m.salt, 'Color', [68 68 253]./255, 'DisplayName', 'OISM (1m)');
+c = plot(yaquina_HT.datetime, yaquina_HT.salt, 'k', 'DisplayName', 'Hatfield High Tide');
 for i = 1:length(casts_HT)  
     
     %%% Plotting high tide cast
@@ -110,6 +93,28 @@ end
 hold off
 xlim([datetime(2020,12,1), datetime(2022,6,1)])
 ylabel('Salinity (psu)');
-legend([a b c d e f], 'Location', 'southeast');
+legend([a b c d e f], 'Location', 'southwest', 'Orientation', 'horizontal');
 
-clear cast_top_salt cast_bottom_salt cast_top cast_bottom i a b c d e
+clear cast_top_salt cast_bottom_salt cast_top cast_bottom i a b c d e f
+
+%%%%%%%%%%%%%%%%%%%%
+%%% Tide Subplot %%%
+%%%%%%%%%%%%%%%%%%%%
+
+ax3 = subplot(413);
+plot(tides.datetime, tides.MSL, 'k');
+xlim([datetime(2020,12,1), datetime(2022,6,1)])
+ylabel('Mean Sea Level (m)');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Yaquina River discharge %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ax4 = subplot(414);
+plot(riverflow.datetime, riverflow.flow, 'k');
+xlim([datetime(2020,12,1), datetime(2022,6,1)])
+ylabel('Yaquina River Discharge (m^3/s)');
+
+%%% Linking axes
+linkaxes([ax1 ax2 ax3 ax4], 'x');
+clear ax1 ax2 ax3 ax4
